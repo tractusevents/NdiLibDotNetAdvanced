64639b69558a1d1cd9e6d9bf4119bcf2290182f9
Elias Puurunen <elias@northernhci.ca>
2025-03-26 09:35:24 -0400

    Added MIT license to advanced wrapper.

--- H264NalParser.cs
+++ H264NalParser.cs
@@ -2,27 +2,6 @@
 using System.Diagnostics;
 
 namespace Tractus.Ndi.Utils;
-
-
-[DebuggerDisplay("NAL Type {NalType} - {Length} at {Offset}")]
-public struct NalUnitIndex
-{
-    /// <summary>
-    /// Offset in the bytestream where the NAL unit begins, including its start code.
-    /// </summary>
-    public int Offset;
-
-    /// <summary>
-    /// Length of the NAL unit data, including the start code.
-    /// </summary>
-    public int Length;
-
-    /// <summary>
-    /// NAL unit type extracted from the header (after the start code).
-    /// </summary>
-    public byte NalType;
-}
-
 public static class H264NalParser
 {
     /// <summary>
---
+++ NalUnitIndex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tractus.Ndi.Utils;
+
+
+[DebuggerDisplay("NAL Type {NalType} - {Length} at {Offset}")]
+public struct NalUnitIndex
+{
+    /// <summary>
+    /// Offset in the bytestream where the NAL unit begins, including its start code.
+    /// </summary>
+    public int Offset;
+
+    /// <summary>
+    /// Length of the NAL unit data, including the start code.
+    /// </summary>
+    public int Length;
+
+    /// <summary>
+    /// NAL unit type extracted from the header (after the start code).
+    /// </summary>
+    public byte NalType;
+}
